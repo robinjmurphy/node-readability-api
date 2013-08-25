805483281dc768c441ffcced6676ce063066c0fd
Robin Murphy <robin@robinmurphy.co.uk>
2013-08-25 12:08:08 +0100

    Renames test classes and moves mocking code into Reader test cases

--- test/reader.js
+++ test/reader.js
@@ -12,14 +12,14 @@ describe('Reader client', function () {
       access_token_secret: 'some_access_token'
     });
 
-    support.initializeMocking();
+    support.resetMocks();
   });
 
   it('can be instantiated with an access token and secret', function () {
     assert.ok(reader);
   });
 
-  it('throws and excepition if an access token and key are missing', function () {
+  it('throws and exception if an access token and key are missing', function () {
     assert.throws(function () {
       var reader = new Reader();
     });
@@ -28,6 +28,8 @@ describe('Reader client', function () {
   describe('User', function () {
 
     it('can get information about the current user', function (done) {
+      support.mockWithFile('GET', '/users/_current', 200);
+
       reader.user(function (err, user) {
         assert.equal(err, null);
         assert.equal(user.username, 'jdoe');
@@ -36,12 +38,21 @@ describe('Reader client', function () {
     });
 
     it('returns an error when the user is not authenticated', function (done) {
-      support.resetMocks();
       support.mockWithContent('GET', '/users/_current', 401, 'Failed to authenticate.');
-
+      
       reader.user(function (err, user) {
         assert.equal(user, undefined);
         assert.equal(err.message, 'HTTP 401: Failed to authenticate.');
+        done();
+      });
+    });
+
+    it('returns an error when the an internal server error occurs', function (done) {
+      support.mockWithContent('GET', '/users/_current', 500, 'Server error.');
+      
+      reader.user(function (err, user) {
+        assert.equal(user, undefined);
+        assert.equal(err.message, 'HTTP 500: Server error.');
         done();
       });
     });
@@ -51,6 +62,8 @@ describe('Reader client', function () {
   describe('Bookmarks', function () {
 
     it('can get all of a user\'s bookmarks', function (done) {
+      support.mockWithFile('GET', '/bookmarks', 200);
+      
       reader.bookmarks({}, function (err, bookmarks) {
         assert.equal(err, null);
         assert.ok(bookmarks.meta);
@@ -62,6 +75,8 @@ describe('Reader client', function () {
     });
 
     it('can get a single bookmark by ID', function (done) {
+      support.mockWithFile('GET', '/bookmarks/75', 200);
+      
       reader.bookmark('75', function (err, bookmark) {
         assert.equal(err, null);
         assert.equal(bookmark.id, '75');
@@ -69,7 +84,34 @@ describe('Reader client', function () {
       });
     });
 
+    it('returns an error when a bookmark cannot be found', function (done) {
+      support.mockWithContent('GET', '/bookmarks/12345', 404, 'Not found.');
+      
+      reader.bookmark('12345', function (err, bookmark) {
+        assert.equal(err.message, 'HTTP 404: Not found.');
+        assert.equal(bookmark, null);
+        done();
+      });
+    });
+
     it('can add a new bookmark', function (done) {
+      support.mockWithHeaders('POST', '/bookmarks', 202,
+        {Location: 'https://www.readability.com/api/rest/v1/bookmarks/75'});
+      support.mockWithFile('GET', '/bookmarks/75', 200);
+
+      reader.addBookmark(
+        'http://some.url.com/article.html', function (err, bookmark) {
+          assert.equal(err, null);
+          assert.equal(bookmark.id, '75');
+          done();
+        });
+    });
+
+    it('returns the existing bookmark when adding a duplicate bookmark', function (done) {
+      support.mockWithHeaders('POST', '/bookmarks', 409,
+        {Location: 'https://www.readability.com/api/rest/v1/bookmarks/75'});
+      support.mockWithFile('GET', '/bookmarks/75', 200);
+
       reader.addBookmark(
         'http://some.url.com/article.html', function (err, bookmark) {
           assert.equal(err, null);
@@ -79,6 +121,8 @@ describe('Reader client', function () {
     });
 
     it('can archive a bookmark', function (done) {
+      support.mockWithFile('POST', '/bookmarks/75', 200);
+
       reader.archiveBookmark('75', function (err, bookmark) {
         assert.equal(err, null);
         assert.equal(bookmark.id, '75');
@@ -87,6 +131,8 @@ describe('Reader client', function () {
     });
 
     it('can unarchive a bookmark', function (done) {
+      support.mockWithFile('POST', '/bookmarks/75', 200);
+
       reader.unarchiveBookmark('75', function (err, bookmark) {
         assert.equal(err, null);
         assert.equal(bookmark.id, '75');
@@ -95,6 +141,8 @@ describe('Reader client', function () {
     });
 
     it('can favourite a bookmark', function (done) {
+      support.mockWithFile('POST', '/bookmarks/75', 200);
+
       reader.favouriteBookmark('75', function (err, bookmark) {
         assert.equal(err, null);
         assert.equal(bookmark.id, '75');
@@ -103,6 +151,8 @@ describe('Reader client', function () {
     });
 
     it('can unfavourite a bookmark', function (done) {
+      support.mockWithFile('POST', '/bookmarks/75', 200);
+
       reader.unfavouriteBookmark('75', function (err, bookmark) {
         assert.equal(err, null);
         assert.equal(bookmark.id, '75');
@@ -114,11 +164,23 @@ describe('Reader client', function () {
       assert.equal(reader.favoriteBookmark, reader.favouriteBookmark);
     });
 
+    it('can remove a bookmark', function (done) {
+      support.mockWithContent('DELETE', '/bookmarks/75', 204, '');
+
+      reader.removeBookmark('75', function (err, success) {
+        assert.equal(err, null);
+        assert.ok(success);
+        done();
+      });
+    });
+
   });
 
   describe('Tags', function () {
 
     it('can get all of the tags for the current user', function (done) {
+      support.mockWithFile('GET', '/tags', 200);
+
       reader.userTags(function (err, tags) {
         assert.equal(err, null);
         assert.equal(tags.length, 4);
@@ -127,6 +189,8 @@ describe('Reader client', function () {
     });
 
     it('can get the tags for a bookmark', function (done) {
+      support.mockWithFile('GET', '/bookmarks/75/tags', 200);
+
       reader.tags('75', function (err, tags) {
         assert.equal(err, null);
         assert.equal(tags.length, 4);
@@ -135,6 +199,8 @@ describe('Reader client', function () {
     });
 
     it('can add a tag to a bookmark', function (done) {
+      support.mockWithFile('POST', '/bookmarks/75/tags', 202);
+
       reader.addTags('75', ['tag1', 'tag2', 'tag3'],
         function (err, tags) {
           assert.equal(err, null);
@@ -143,7 +209,20 @@ describe('Reader client', function () {
         });
     });
 
+    it('returns an error if the tag limit is reached', function (done) {
+      support.mockWithContent('POST', '/bookmarks/75/tags', 403, 'No more Tags can be added to this Bookmark.');
+
+      reader.addTags('75', ['tag1', 'tag2', 'tag3'],
+        function (err, tags) {
+          assert.equal(err.message, 'HTTP 403: No more Tags can be added to this Bookmark.');
+          assert.equal(tags, undefined);
+          done();
+        });
+    });
+
     it('can remove a tag from a bookmark', function (done) {
+      support.mockWithContent('DELETE', '/bookmarks/75/tags/123', 204, '');
+      
       reader.removeTag('75', '123',
         function (err, success) {
           assert.equal(err, null);
@@ -157,6 +236,8 @@ describe('Reader client', function () {
   describe('Articles', function () {
 
     it('can get an article by its ID', function (done) {
+      support.mockWithFile('GET', '/articles/47g6s8e7', 200);
+
       reader.article('47g6s8e7', function (err, article) {
         assert.equal(err, null);
         assert.equal(article.id, '47g6s8e7');
--- test/support.js
+++ test/support.js
@@ -1,7 +1,7 @@
 var nock = require('nock');
 var BASE_URL = 'https://www.readability.com/api/rest/v1';
 
-function intercept(method, api_path) {
+function intercept(api_path, method) {
   var scope = nock(BASE_URL)
       .filteringPath(function (path) {
         if (path.match(new RegExp(api_path + '$'))) {
@@ -14,31 +14,17 @@ function intercept(method, api_path) {
 }
 
 module.exports.mockWithFile = function (method, api_path, statusCode) {
-  intercept(method, api_path).replyWithFile(statusCode, __dirname + '/fixtures' + api_path + '.json');
+  intercept(api_path, method).replyWithFile(statusCode, __dirname + '/fixtures' + api_path + '.json');
 };
 
 module.exports.mockWithHeaders = function (method, api_path, statusCode, headers) {
-  intercept(method, api_path).reply(statusCode, '', headers);
+  intercept(api_path, method).reply(statusCode, '', headers);
 };
 
 module.exports.mockWithContent = function (method, api_path, statusCode, content) {
-  intercept(method, api_path).reply(statusCode, content);
+  intercept(api_path, method).reply(statusCode, content);
 };
 
 module.exports.resetMocks = function () {
   nock.cleanAll();
-};
-
-module.exports.initializeMocking = function () {
-  this.mockWithFile('GET', '/users/_current', 200);
-  this.mockWithFile('GET', '/bookmarks/75', 200);
-  this.mockWithFile('POST', '/bookmarks/75', 200);
-  this.mockWithFile('GET', '/tags', 200);
-  this.mockWithFile('GET', '/bookmarks/75/tags', 200);
-  this.mockWithFile('POST', '/bookmarks/75/tags', 200);
-  this.mockWithFile('GET', '/articles/47g6s8e7', 200);
-  this.mockWithContent('DELETE', '/bookmarks/75/tags/123', 204, '');
-  this.mockWithContent('DELETE', '/bookmarks/75', 204, '');
-  this.mockWithFile('GET', '/bookmarks', 200);
-  this.mockWithHeaders('POST', '/bookmarks', 202, {Location: BASE_URL + '/bookmarks/75'});
 };
